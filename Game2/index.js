a9a621b0fad919d357995e83ad25ff2966e62681
RRTropical <lorenzocooks@gmail.com>
2024-02-10 15:03:39 -0500

    Undo

--- Game2/index.js
+++ Game2/index.js
@@ -5,32 +5,7 @@ let figureElement = null
 
 gasButton.addEventListener('click', getMovie)
 trashButton.addEventListener('click', getMovie)
-const steamApiKey = '9837A57C70935C37B23D7F3C09AEE0E4';
 
-async function getRandomVRGameName() {
-  try {
-    // Get the list of VR games on Steam
-    const response = await fetch(`https://api.steampowered.com/ISteamApps/GetAppList/v2/`);
-    const data = await response.json();
-
-    // Filter the list to include only VR games
-    const vrGames = data.applist.apps.filter(app => app && app.genres && app.genres.some(genre => genre.description === 'Virtual Reality'));
-
-    if (vrGames.length > 0) {
-      // Get a random VR game name
-      const randomIndex = Math.floor(Math.random() * vrGames.length);
-      const randomVRGameName = vrGames[randomIndex].name;
-
-      console.log(`Random VR Game Name: ${randomVRGameName}`);
-    } else {
-      console.error('No VR games found in the Steam API response.');
-    }
-  } catch (error) {
-    console.error('Error fetching random VR game name:', error);
-  }
-}
-
-getRandomVRGameName();
 async function getMovie() {
   const apiKey = '43245e4a7672fe10cdaec2ec5bd00037'
   const apiUrl = 'https://api.themoviedb.org/3/discover/movie'
